9b694bae1c6f1497ba83f8ab6e5b7925ef36b4c5
mayard689 <doc-albert@laposte.net>
2020-04-05 21:48:05 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+require '../vendor/autoload.php';
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+$loader = new \Twig\Loader\FilesystemLoader('../src/View');
+$twig = new \Twig\Environment($loader);
+echo $twig->render('index.html.twig', array(
+    'products'=>$products
+));
+
